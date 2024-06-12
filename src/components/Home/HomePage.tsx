b0ae6278b0dbf7619c19f698910f91de2f2631b0
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-12 20:00:00 +0600

    update HomePage

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -8,14 +8,14 @@ import Features from "../ui/Features";
 import Statics from "../ui/Statics";
 
 const HomePage = () => {
-  const [jobs, setJobs] = useState<null | any>([]);
+  const [course, setCourse] = useState<null | any>([]);
 
   useEffect(() => {
     const baseUrl = getBaseUrl();
 
     fetch(`${baseUrl}/course`)
       .then((res) => res.json())
-      .then((data) => setJobs(data));
+      .then((data) => setCourse(data));
   }, []);
 
   return (
@@ -23,10 +23,10 @@ const HomePage = () => {
       <Banner />
       <div className="mt-12 container mx-auto">
         <div className="m-2">
-          <h1 className="text-3xl font-bold mb-3 text-center">Recent Jobs</h1>
+          <h1 className="text-3xl font-bold mb-3 text-center">Recent Course</h1>
           <div className="grid grid-cols-1 gap-4">
-            {jobs.slice(0, 2).map((job: any) => (
-              <CourseCard key={job?.id} course={job} />
+            {course.slice(0, 2).map((course: any) => (
+              <CourseCard key={course?.id} course={course} />
             ))}
           </div>
           <Features />

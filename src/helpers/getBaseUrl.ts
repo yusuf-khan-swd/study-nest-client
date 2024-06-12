bdc969e237e5c33ba569fa4e9a3e6f693af0bcff
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-12 14:12:46 +0600

    add server url to getBaseUrl() function

--- src/helpers/getBaseUrl.ts
+++ src/helpers/getBaseUrl.ts
@@ -1,5 +1,5 @@
 export const getBaseUrl = (): string => {
   return process.env.NODE_ENV === "development"
     ? "http://localhost:5000"
-    : "https://career-nest-server-web.vercel.app";
+    : "https://study-nest-server.vercel.app/";
 };

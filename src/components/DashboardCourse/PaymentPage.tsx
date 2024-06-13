f006a6e188c2d1462e1e24548eb120fc8d6acb45
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-13 20:29:25 +0600

    use loading and LoadingSpinner component

--- src/components/DashboardCourse/PaymentPage.tsx
+++ src/components/DashboardCourse/PaymentPage.tsx
@@ -5,12 +5,14 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import LoadingSpinner from "../ui/LoadingSpinner";
 import CheckOutForm from "./CheckoutForm";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PK as string);
 
 const PaymentPage = ({ id }: { id: string }) => {
   const [courseInfo, setCourseInfo] = useState<null | any>({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const baseUrl = getBaseUrl();
@@ -18,13 +20,23 @@ const PaymentPage = ({ id }: { id: string }) => {
     fetch(`${baseUrl}/course/${id}`)
       .then((res) => res.json())
       .then((result) => {
+        setLoading(false);
         if (result) {
           setCourseInfo(result);
         } else {
           toast.error("Course data failed to get");
         }
+      })
+      .catch((error) => {
+        setLoading(false);
+        toast.error(error?.message);
+        console.log("Error: ", error);
       });
   }, [id]);
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
 
   return (
     <div className="card w-full bg-base-100 shadow-xl border">

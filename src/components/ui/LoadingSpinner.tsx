be2b2955c5b66c3e8f9fb744b6ea503a3c9c536c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-13 22:20:32 +0600

    make loading spinner responsive

--- src/components/ui/LoadingSpinner.tsx
+++ src/components/ui/LoadingSpinner.tsx
@@ -1,9 +1,9 @@
 const LoadingSpinner = () => {
   return (
     <div className="flex justify-center items-center h-[calc(100vh-88px)]">
-      <p className="text-7xl font-bold">L</p>
-      <div className="w-12 h-12 border-8 border-dashed rounded-full animate-spin mt-5 border-red-500"></div>
-      <p className="text-7xl font-bold">Loading....</p>
+      <p className="text-lg sm:text-7xl font-bold">L</p>
+      <div className="w-4 h-4 sm:w-12 sm:h-12 border-2 sm:border-8 border-dashed rounded-full animate-spin sm:mt-5 border-red-500"></div>
+      <p className="text-lg sm:text-7xl font-bold">ading....</p>
     </div>
   );
 };

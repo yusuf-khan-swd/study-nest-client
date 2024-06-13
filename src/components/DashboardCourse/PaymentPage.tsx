50c9ddd2f9bef40f7f4f186d45f013cc0ef6f096
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-13 20:02:54 +0600

    add PaymentPage component with element from stripe and stripPromise with stripe private key

---
+++ src/components/DashboardCourse/PaymentPage.tsx
@@ -0,0 +1,36 @@
+import { getBaseUrl } from "@/helpers/getBaseUrl";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import CheckOutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PK as string);
+
+const PaymentPage = ({ id }: { id: string }) => {
+  const [courseInfo, setCourseInfo] = useState<null | any>({});
+
+  useEffect(() => {
+    const baseUrl = getBaseUrl();
+
+    fetch(`${baseUrl}/course/${id}`)
+      .then((res) => res.json())
+      .then((result) => {
+        if (result) {
+          setCourseInfo(result);
+        } else {
+          toast.error("Course data failed to get");
+        }
+      });
+  }, [id]);
+
+  return (
+    <div className="card w-full bg-base-100 shadow-xl border">
+      <Elements stripe={stripePromise}>
+        <CheckOutForm course={courseInfo}></CheckOutForm>
+      </Elements>
+    </div>
+  );
+};
+
+export default PaymentPage;

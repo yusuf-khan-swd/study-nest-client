37db5c236a965053b1d39da6605d68a0500fce1a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-14 11:16:24 +0600

    add a setToken() function to set token to localStorage

--- src/helpers/token.ts
+++ src/helpers/token.ts
@@ -1,5 +1,9 @@
 const TOKEN_KEY = "token";
 
+export const setToken = (token: string) => {
+  localStorage.setItem(TOKEN_KEY, token);
+};
+
 export const getToken = () => {
   return localStorage.getItem(TOKEN_KEY);
 };

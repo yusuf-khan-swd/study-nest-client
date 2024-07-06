77781c3417037ed300fc8fd21f5d5e4fdb62bb15
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-07-06 11:46:09 +0600

    use form inside card body by wrapping the form fields

--- src/components/Logins/LoginPage.tsx
+++ src/components/Logins/LoginPage.tsx
@@ -56,7 +56,7 @@ const LoginPage = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="hero min-h-[80vh] bg-base-200">
+    <div className="hero min-h-[80vh] bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse max-w-[1040px] mx-auto">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Login now!</h1>
@@ -68,38 +68,40 @@ const LoginPage = () => {
         </div>
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <div className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                placeholder="email"
-                className="input input-bordered"
-                name="email"
-                required
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                placeholder="password"
-                className="input input-bordered"
-                name="password"
-                required
-              />
-            </div>
+            <form onSubmit={handleSubmit}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  name="email"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  name="password"
+                  required
+                />
+              </div>
 
-            <div className="form-control mt-6">
-              <input
-                className="btn bg-red-500 text-white"
-                type="submit"
-                value="Login"
-              />
-            </div>
+              <div className="form-control mt-6">
+                <input
+                  className="btn bg-red-500 text-white"
+                  type="submit"
+                  value="Login"
+                />
+              </div>
+            </form>
             <div className="mt-6">
               <GoogleLogin />
             </div>
@@ -114,7 +116,7 @@ const LoginPage = () => {
           </div>
         </div>
       </div>
-    </form>
+    </div>
   );
 };
 

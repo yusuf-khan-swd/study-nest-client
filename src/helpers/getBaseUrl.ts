03792bbbd08c723faf916781998ccfdf826983af
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-02-23 11:49:38 +0600

    done checking routes for test deployed server with client side

--- src/helpers/getBaseUrl.ts
+++ src/helpers/getBaseUrl.ts
@@ -1,5 +1,6 @@
 export const getBaseUrl = (): string => {
-  return process.env.NODE_ENV === "development"
-    ? "http://localhost:5000"
-    : "https://study-nest-server.vercel.app";
+  // return process.env.NODE_ENV === "development"
+  //   ? "http://localhost:5000"
+  //   : "https://study-nest-server.vercel.app";
+  return "https://study-nest-test-server.vercel.app";
 };

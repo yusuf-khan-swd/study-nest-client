67a50e0a4d03f96ce05cffb59c991f57b222f997
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-02-23 11:58:07 +0600

    back base url conditional render

--- src/helpers/getBaseUrl.ts
+++ src/helpers/getBaseUrl.ts
@@ -1,6 +1,5 @@
 export const getBaseUrl = (): string => {
-  // return process.env.NODE_ENV === "development"
-  //   ? "http://localhost:5000"
-  //   : "https://study-nest-server.vercel.app";
-  return "https://study-nest-server.vercel.app";
+  return process.env.NODE_ENV === "development"
+    ? "http://localhost:5000"
+    : "https://study-nest-server.vercel.app";
 };

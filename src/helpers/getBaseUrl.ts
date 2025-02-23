ea399089292cceeeefbdd18daa01ac47a8e0d558
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-02-23 11:56:28 +0600

    Done] - Connect original deploy server to client side and check routes are working properly

--- src/helpers/getBaseUrl.ts
+++ src/helpers/getBaseUrl.ts
@@ -2,5 +2,5 @@ export const getBaseUrl = (): string => {
   // return process.env.NODE_ENV === "development"
   //   ? "http://localhost:5000"
   //   : "https://study-nest-server.vercel.app";
-  return "https://study-nest-test-server.vercel.app";
+  return "https://study-nest-server.vercel.app";
 };
